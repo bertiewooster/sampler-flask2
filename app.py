ae03a5690e7566389f41ed8c02485d75aa4378d4
bertiewooster <jemonat@calalum.org>
2021-06-19 23:27:05 -0400

    Works using upload_file_plus.html

--- app.py
+++ app.py
@@ -15,6 +15,41 @@ def hello_world():
     # Upload a new file
     # Download an existing file--list the files in 
 
+# Run sampler API
+@app.route('/run', methods=['GET', 'POST'])
+def run():
+    print('Running run')
+    if request.method == 'POST':
+        print('POST')
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('no file')
+            return redirect(request.url)
+        print('A file was provided')
+        file = request.files['file']
+        print('file.filename = {}'.format(file.filename))
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('no filename')
+            return redirect(request.url)
+        else:
+            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+            # ensure the filename is safe to use
+            filename = secure_filename(file.filename)
+            # compose file path: upload folder + filename
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            # save file
+            file.save(filepath)
+            print("saved file successfully")
+            # send file name as parameter to download
+            return redirect('/downloadfile/' + filename)
+
+    #return render_template('run.html')
+    #return render_template('upload_file.html')
+    #return render_template('image_upload.html')
+    return render_template('upload_file_plus.html')
+
 # Upload API
 @app.route('/uploadfile', methods=['GET', 'POST'])
 def upload_file():
@@ -41,7 +76,8 @@ def upload_file():
             # send file name as parameter to download
             return redirect('/downloadfile/' + filename)
 
-    return render_template('upload_file.html')
+    #return render_template('upload_file.html')
+    #return render_template('run.html')
 
 # Download API
 @app.route("/downloadfile/<filename>", methods = ['GET'])
